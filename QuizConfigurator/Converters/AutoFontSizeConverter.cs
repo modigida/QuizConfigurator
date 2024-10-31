154b54227534081e19bb48eabd57cec58f32cb46
modigida <modigida@hotmail.com>
2024-10-31 17:05:27 +0100

    Adapt PlayerView layout depending on screen size

---
+++ QuizConfigurator/Converters/AutoFontSizeConverter.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace QuizConfigurator.Converters;
+public class AutoFontSizeConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is string text)
+        {
+            // Här kan du justera min/max storlek och logik för att beräkna storlek
+            if (text.Length > 30) // Justera detta värde beroende på vad som anses "för lång"
+            {
+                return 24; // Minska storleken
+            }
+            return 34; // Standard storlek
+        }
+        return 34; // Standard storlek om inget värde
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
